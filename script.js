77ebd91f2e9cce71636adcb628c625501fe1b8c1
Juan Gonzalez <gonzalesjuandavid608@gmail.com>
2023-02-17 11:00:10 -0500

    Update script.js with localStorage

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
+const body = document.querySelector('body');
 const theme = document.getElementById('theme');
 
-theme.addEventListener('change', () => {
-  document.body.classList.toggle('light');
+theme.addEventListener('click', () => {
+  body.classList.toggle('light');
+
+  // Guardamos el modo en localstorage.
+	if (body.classList.contains('light')) {
+		localStorage.setItem('light-mode', 'true');
+	} else {
+		localStorage.setItem('light-mode', 'false');
+	}
 });
+
+// Obtenemos el modo actual.
+if (localStorage.getItem('light-mode') === 'true') {
+	body.classList.add('light');
+} else {
+  body.classList.remove('light');
+}
